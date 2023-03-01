fe97e1c2c9798da41e014920138ac2f7955b5066
Vitoria Oliveira <vitoria.oli.san14@gmail.com>
2023-03-01 10:00:34 +0100

    create perfil controller

--- src/main/java/smile/example/smile/controller/PerfilController.java
+++ src/main/java/smile/example/smile/controller/PerfilController.java
@@ -1,5 +1,24 @@
 package smile.example.smile.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import smile.example.smile.service.CiudadService;
+import smile.example.smile.service.PerfilService;
+import smile.example.smile.service.TratamientoService;
+
+@RestController
+@RequestMapping(path = "/api/perfil")
+
 public class PerfilController {
+    private PerfilService service;
+    private CiudadService ciudadService;
+
+    
+    public PerfilController(PerfilService service, CiudadService ciudadService) {
+        this.service = service;
+        this.ciudadService = ciudadService;
+    }
+
     
 }
